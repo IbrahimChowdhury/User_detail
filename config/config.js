b2c8793b74dbea6ddc2a863ade549dfa997d95f7
Ibrahim Chowdhury <ibrahimchowdhury1000@gmail.com>
2023-01-15 03:03:16 +0600

    localserver deleted

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ let dev={
         port:process.env.port || 4000
     },
     db:{
-        url: process.env.DB_URL || "mongodb://127.0.0.1:27017/UserDemoDb"
+        url: process.env.DB_URL 
     }
 }
 
